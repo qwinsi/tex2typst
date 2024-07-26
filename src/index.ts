94ec6b2ddc97d706139763bb40d8d5fc07ca84ee
wwc936 <wwc936@outlook.com>
2024-07-26 08:04:54 +0800

    allow nonStrict mode

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
 import { parseTex } from "./parser";
-import { Tex2TypstSettings } from "./types";
+import { Tex2TypstOptions } from "./types";
 import { TypstWriter } from "./writer";
 
-export function tex2typst(tex: string, settings?: Tex2TypstSettings): string {
-    const DEFAULT_SETTINGS: Tex2TypstSettings = {
+export function tex2typst(tex: string, options?: Tex2TypstOptions): string {
+    const opt: Tex2TypstOptions = {
+        nonStrict: false,
         preferTypstIntrinsic: false,
         customTexMacros: {}
     };
-    if (!settings) {
-        settings = DEFAULT_SETTINGS;
-    } else {
-        if (!settings.preferTypstIntrinsic) {
-            settings.preferTypstIntrinsic = DEFAULT_SETTINGS.preferTypstIntrinsic;
+    if (options) {
+        if (options.nonStrict) {
+            opt.nonStrict = options.nonStrict;
         }
-        if (!settings.customTexMacros) {
-            settings.customTexMacros = DEFAULT_SETTINGS.customTexMacros;
+        if (options.preferTypstIntrinsic) {
+            opt.preferTypstIntrinsic = options.preferTypstIntrinsic;
+        }
+        if (options.customTexMacros) {
+            opt.customTexMacros = options.customTexMacros;
         }
     }
-    const t = parseTex(tex, settings.customTexMacros!);
-    const writer = new TypstWriter(settings.preferTypstIntrinsic!);
+    const t = parseTex(tex, opt.customTexMacros!);
+    const writer = new TypstWriter(opt.nonStrict!, opt.preferTypstIntrinsic!);
     writer.append(t);
     return writer.finalize();
 }
